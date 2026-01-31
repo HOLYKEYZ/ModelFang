bee9777438bcf1c389dcf8493ee1088f694ecfa9
HOLYKEYZ <ayandajoseph390@gmail.com>
2026-01-31 21:31:00 +0100

    chore: Remove an unspecified file.

--- frontend/app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from '@clerk/nextjs';
-
-export default function Page() {
-  return (
-    <div className="min-h-screen bg-black flex items-center justify-center">
-      <SignIn />
-    </div>
-  );
-}
